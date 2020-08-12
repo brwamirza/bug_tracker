9d70b5c4c18304594b5d9017ad03b69b662967fd
Your Name <you@example.com>
2020-08-12 14:30:59 +0300

    created signin and signup component

---
+++ src/components/signin.component.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import '../css/signin.css';
+
+
+export default class Signin extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+     <div>
+         <h1>hello world</h1>
+     </div>
+    );
+  }
+}
---
+++ src/components/signup.component.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import '../css/signup.css';
+
+
+export default class Signup extends Component {
+  constructor(props) {
+    super(props);
+   }
+
+  render() {
+    return (
+     <div>
+         
+     </div>
+    );
+  }
+}
