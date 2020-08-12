8c8a45094599beca26ad5137576af85ecf37781e
Your Name <you@example.com>
2020-08-12 16:27:42 +0300

    adding signin component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Signin from './components/signin.component';
 import Signup from './components/signup.component';
 
--- src/components/signin.component.js
+++ src/components/signin.component.js
@@ -10,9 +10,33 @@ export default class Signin extends Component {
 
   render() {
     return (
-     <div>
-         <h1>hello world</h1>
-     </div>
+     <div id="signin">
+        <img class="home__shape" src={require("../circle_transparent.svg")}/>
+        <div className="home__left">
+            <div class="home__text">
+                <h1 class="text--light">Track</h1>
+                <h1 class="text--light">Manage</h1>
+                <h1 class="text--light">& Kill Bugs</h1>
+                <h1 class="text--bold">Effectively</h1>
+            </div>
+        </div>
+
+        <div className="home__right">
+            <div className="signin_wrapper">
+                <div className="form">
+                    <h1 class="text--bold">Login</h1>
+                    <h2 class="text--bold">Welcome back!</h2>
+                    <form action="">
+                        
+                    </form>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
     );
   }
 }
