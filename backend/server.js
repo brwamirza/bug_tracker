6e8509f92993f4db64817000c78a24a4dfaff76d
Your Name <you@example.com>
2020-08-13 15:36:26 +0300

    adding role based auth

--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,28 @@
-const express = require('express');
-const cors = require('cors');
-
-require('dotenv').config();
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 
 const app = express();
-const port = process.env.PORT || 5000;
 
-app.use(cors());
-app.use(express.json());
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
 
-app.listen(port, () => {
-    console.log(`Server is running on port: ${port}`);
+app.use(cors(corsOptions));
+
+// parse requests of content-type - application/json
+app.use(bodyParser.json());
+
+// parse requests of content-type - application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// simple route
+app.get("/", (req, res) => {
+  res.json({ message: "Welcome to bezkoder application." });
+});
+
+// set port, listen for requests
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
 });
